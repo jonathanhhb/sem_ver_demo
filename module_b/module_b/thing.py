63684a4187ba3a476701ab30c3b72277ae9ab783
Jonathan Bloedow <jbloedow@idmod.org>
2021-11-13 14:42:24 -0800

    0.1.0.

--- module_b/module_b/thing.py
+++ module_b/module_b/thing.py
@@ -1,3 +1,6 @@
 def foo():
     print( "Module B Third Implementation. Patched another implementation bug." )
     return
+
+def foo2():
+    print( "This is a new function. This means we need a minor version bump." )
